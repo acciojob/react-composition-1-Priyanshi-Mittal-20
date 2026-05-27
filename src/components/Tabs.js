4d3742af2e4db691d18a32601a9f7b6f9bddca5e
Priyanshi Mittal <priyanshimzn05@gmail.com>
2026-05-27 18:18:09 +0530

    tab-1

---
+++ src/components/Tabs.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Tabs = ({tabs}) => {
+  const [content,setContent]=useState("")
+  return (
+    <div>
+      <ul>
+      {
+        tabs.map(tab=><li><a onClick={()=>setContent(tab.content)}>{tab.title}</a></li>)
+      }
+      </ul>
+      {content && <p>This is the content for {content}.</p>}
+    </div>
+  )
+}
+
+export default Tabs
