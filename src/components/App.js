136a4eb55daa97663924294f9c70418fa921f2b4
Priyanshi Mittal <priyanshimzn05@gmail.com>
2026-05-28 10:56:29 +0530

    tabs-3

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,9 @@ import React from "react";
 import './../styles/App.css';
 import Tabs from "./Tabs";
 
-const tabs=[{title:"Tab 1", content:"This is the content Tab 1."},
-  {title:"Tab 2", content:"This is the content Tab 2."},
-  {title:"Tab 3", content:"This is the content Tab 3."}
+const tabs=[{title:"Tab 1", content:"This is the content for Tab 1."},
+  {title:"Tab 2", content:"This is the content for Tab 2."},
+  {title:"Tab 3", content:"This is the content for Tab 3."}
 ]
 
 const App = () => {
